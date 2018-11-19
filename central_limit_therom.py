826df96d23b2379d66daf3121950bc4dde705a37
Catherine Huang <39412642+catherhuang@users.noreply.github.com>
2018-11-18 23:05:06 -0500

    added mmm
    mean median mode added

--- central_limit_therom.py
+++ central_limit_therom.py
@@ -14,3 +14,43 @@ mean_eager = float(input())
 std_eager = float(input())
 cdf = (1+ math.erf((total_ticket - (mean_eager*eager_student)) / (std_eager*math.sqrt(eager_student)*math.sqrt(2)))) * 0.5
 print(round(cdf,4))
+
+
+
+nums=int(input())
+num_lst=list(map(int, input().split()))
+
+def mean(nums): 
+    sum = 0 
+    for i in nums: 
+        sum = sum + i 
+    mean = float(sum) / len (nums)
+    return mean
+
+def median(nums): 
+    sorted_nums= sorted(nums)
+    size_nums=len(nums)
+    index = (size_nums - 1) // 2
+    if (size_nums % 2 !=0):
+        median= sorted_nums[index]
+    else:
+        median= (sorted_nums[index] + sorted_nums[index + 1])/2.0
+    return median
+
+def mode(nums): 
+    mode = 0
+    size = len(nums)
+    count, max = 0, 0
+    copy = sorted(nums) 
+    current = 0
+    for i in copy:
+        if (i == current):
+            count += 1
+        else:
+            count = 1
+            current = i
+        if (count > max):
+            max = count
+            mode = i
+    return mode
+
