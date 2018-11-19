a50ce8a0b9fd336f517ee18dbd70cbb0d252fbcf
Catherine Huang <39412642+catherhuang@users.noreply.github.com>
2018-11-18 23:04:20 -0500

    another solution
    added another solution for central limit theorem

--- central_limit_therom.py
+++ central_limit_therom.py
@@ -1,6 +1,16 @@
 import math
 
+#central limit therome 
+
 def less_than_boundary_cdf(x, mean, std):
     return round(0.5 * (1 + math.erf((x - mean)/ (std * math.sqrt(2)))), 4)
 
 print( less_than_boundary_cdf(9800, 49 * 205, math.sqrt(49) * 15))
+
+
+total_ticket = int(input())
+eager_student = int(input())
+mean_eager = float(input())
+std_eager = float(input())
+cdf = (1+ math.erf((total_ticket - (mean_eager*eager_student)) / (std_eager*math.sqrt(eager_student)*math.sqrt(2)))) * 0.5
+print(round(cdf,4))
